32851cfd745bad1cc71c5a0fc0598e505132659c
IreneMoradi <irenemoradii@gmail.com>
2025-11-11 12:16:01 +0100

    Initial Commit

---
+++ MyBookApp/MyBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookAppApp.swift
+//  MyBookApp
+//
+//  Created by Irene Mordi on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
